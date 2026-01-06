97552f379cbe154ed03dd1dfc5597f05c74d34b9
Skyron <courtier.leo@gmail.com>
2026-01-07 00:08:55 +0100

    Add "Create category" button

--- Kraken/Components/Category.razor.cs
+++ Kraken/Components/Category.razor.cs
@@ -5,5 +5,4 @@ namespace Kraken.Components;
 public partial class Category : ComponentBase
 {
     private string _name="This is my category";
-    
 }
---
+++ Kraken/Components/CategoryButton.razor.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Kraken.Components;
+
+public partial class CategoryButton : ComponentBase
+{
+    [Parameter] public bool IsOpen { get; set; }
+    [Parameter] public EventCallback<bool> IsOpenChanged { get; set; }
+    
+    async Task DisplayForm()
+    {
+        IsOpen = !IsOpen;
+        await IsOpenChanged.InvokeAsync(IsOpen);
+    }
+}
